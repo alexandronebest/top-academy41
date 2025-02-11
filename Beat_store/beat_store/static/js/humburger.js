c6deed886b0eae96f57fc509502ca2a1bd0fcbd4
alexandronebest <alex-domrachev@mail.ru>
2025-02-11 21:41:51 +0700

    на уроке

--- Beat_store/beat_store/static/js/humburger.js
+++ Beat_store/beat_store/static/js/humburger.js
@@ -1,11 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const navbarToggler = document.querySelector('.navbar-toggler');
-//     const navbarCollapse = document.querySelector('.navbar-collapse');
 
-//     navbarToggler.addEventListener('click', function () {
-//         navbarCollapse.classList.toggle('show');
-//     });
-// });
 document.addEventListener('DOMContentLoaded', function () {
     const navbarToggler = document.querySelector('.navbar-toggler');
     const navbarCollapse = document.querySelector('.navbar-collapse');
